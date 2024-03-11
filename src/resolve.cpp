68880dcb71b02cba359e1b7edb174db5b0dc4ebb
tttm <80350771+33tm@users.noreply.github.com>
2024-03-10 18:39:52 -0700

    array to vector + cmake

--- src/resolve.cpp
+++ src/resolve.cpp
@@ -16,8 +16,8 @@ static std::string parse(auto input) {
 }
 
 std::vector<Resolution> resolve(const std::vector<Package> &packages) {
-    CURL *handles[packages.size()];
     CURLM *curlm = curl_multi_init();
+    std::vector<CURL *> handles(packages.size());
     std::vector<std::string> responses(packages.size());
     std::vector<Resolution> resolutions(packages.size());
 
@@ -31,15 +31,13 @@ std::vector<Resolution> resolve(const std::vector<Package> &packages) {
     for (size_t i = 0; i < packages.size(); i++) {
         handles[i] = curl_easy_init();
         std::string url = "https://registry.npmjs.org/" + packages[i].name + "/" + packages[i].version;
-
         curl_easy_setopt(handles[i], CURLOPT_URL, url.c_str());
         curl_easy_setopt(handles[i], CURLOPT_WRITEDATA, &responses[i]);
         curl_easy_setopt(handles[i], CURLOPT_WRITEFUNCTION, *write);
-
         curl_multi_add_handle(curlm, handles[i]);
     }
 
-    int remaining;
+    int remaining = 0;
 
     while (remaining) {
         if (curl_multi_perform(curlm, &remaining)) {
@@ -68,6 +66,8 @@ std::vector<Resolution> resolve(const std::vector<Package> &packages) {
         // resolution.dependencies = std::vector<Package>();
 
         resolutions.push_back(resolution);
+
+        std::cout << resolution.name << "@" << resolution.version << std::endl;
     }
 
     return resolutions;
