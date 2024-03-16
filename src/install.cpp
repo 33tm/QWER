21d07b58c5018e7896b88320faa0ae79b653bc08
tttm <80350771+33tm@users.noreply.github.com>
2024-03-16 11:38:49 -0700

    spinner update

--- src/install.cpp
+++ src/install.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <iomanip>
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -10,21 +11,94 @@ void install(const std::vector<Package> &packages) {
     std::vector<Resolution> resolutions;
 
     std::thread spinner([&resolutions, &packages] {
-        while (true) {
-            for (Package package : packages) {
-                std::cout << package.name << std::right
-                          << std::setw(50 - package.name.size())
-                          << "/" << std::endl;
-            }
-            if (!resolutions.empty()) {
-                break;
-            }
-            std::cout << "\033[" << packages.size() << "A";
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        }
+        size_t i = 0;
+        const char *frames[] = {
+            "⢀⠀",
+            "⡀⠀",
+            "⠄⠀",
+            "⢂⠀",
+            "⡂⠀",
+            "⠅⠀",
+            "⢃⠀",
+            "⡃⠀",
+            "⠍⠀",
+            "⢋⠀",
+            "⡋⠀",
+            "⠍⠁",
+            "⢋⠁",
+            "⡋⠁",
+            "⠍⠉",
+            "⠋⠉",
+            "⠋⠉",
+            "⠉⠙",
+            "⠉⠙",
+            "⠉⠩",
+            "⠈⢙",
+            "⠈⡙",
+            "⢈⠩",
+            "⡀⢙",
+            "⠄⡙",
+            "⢂⠩",
+            "⡂⢘",
+            "⠅⡘",
+            "⢃⠨",
+            "⡃⢐",
+            "⠍⡐",
+            "⢋⠠",
+            "⡋⢀",
+            "⠍⡁",
+            "⢋⠁",
+            "⡋⠁",
+            "⠍⠉",
+            "⠋⠉",
+            "⠋⠉",
+            "⠉⠙",
+            "⠉⠙",
+            "⠉⠩",
+            "⠈⢙",
+            "⠈⡙",
+            "⠈⠩",
+            "⠀⢙",
+            "⠀⡙",
+            "⠀⠩",
+            "⠀⢘",
+            "⠀⡘",
+            "⠀⠨",
+            "⠀⢐",
+            "⠀⡐",
+            "⠀⠠",
+            "⠀⢀",
+            "⠀⡀",
+            "  ",
+            "  ",
+            "  ",
+            "  ",
+            "  ",
+            "  ",
+            "  ",
+            "  ",
+        };
+        // while (resolutions.empty()) {
+        //     for (Package package : packages) {
+        //         std::cout << package.name << std::right
+        //                   << std::setw(50 - package.name.size())
+        //                   << frames[i] << std::endl;
+        //     }
+        //     i = (i + 1) % 64;
+        //     std::cout << "\033[" << packages.size() << "A";
+        //     std::cout << package.name << std::right
+        //     std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        // }
+        // for (Package package : packages) {
+        //               << std::setw(50 - package.name.size())
+        //               << "|" << std::endl;
+        // }
     });
 
     resolutions = resolve(packages);
+
+    for (const Resolution &resolution : resolutions) {
+    }
 
     spinner.join();
 }
